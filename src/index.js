df9a4256230bfc4ea5de0dec16b9657a582da5dd
aditya dubey <48158220+dubeyaditya29@users.noreply.github.com>
2021-09-28 00:35:28 +0530

    fix fixed some issues

--- src/index.js
+++ src/index.js
@@ -8,21 +8,21 @@ const notes = [2000, 500, 200, 100, 50, 20, 10, 1];
 const notes_area = document.querySelectorAll(".number-of-notes");
 button.addEventListener("click", function calculateAmountToBeReturned() {
   if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
+    if (cashGiven.value <= billAmount.value) {
       let amountToBeReturned = cashGiven.value - billAmount.value;
       console.log(amountToBeReturned);
-      calulateChange(amountToBeReturned);
+      calculateChange(amountToBeReturned);
     } else {
       out.style.display = "block";
-      out.innerText = "Bartan Dhona hai?";
+      out.innerText = "given amount should always be greater";
     }
   } else {
     out.style.display = "block";
-    out.innerText = "tera khata clear hai!";
+    out.innerText = "Noting to pay!";
   }
 });
 
-function calulateChange(amountReturnValue) {
+function calculateChange(amountReturnValue) {
   for (let i = 0; i < notes.length; i++) {
     let note_value = Math.trunc(amountReturnValue / notes[i]);
     amountReturnValue %= notes[i];
